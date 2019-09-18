73ae1ab60ba85453cff4690055c78233d0595a40
makushu <phatumakushu@gmail.com>
2019-09-18 16:24:41 +0200

    Update passwordSpec.js

--- spec/passwordSpec.js
+++ spec/passwordSpec.js
@@ -1,34 +1,26 @@
-
-describe("password_is_valid", function(){
-
-    it ("should have more than 8 characters", function(){
-        expect(password_is_valid("12345ABcde")).toBe(true);
+describe("password_is_valid", () => {
+   
+    it("should check if password is longer than 8 characters", () => {
+      expect(() => {password_is_valid("QLjghH2")}).toThrow(new Error("password should be larger than 8 characters"));
     });
 
-    it ("should not be null", function(){
-        expect(password_is_valid("ABcde12345")).toBe(true);
+    it("should check if password is not null", () => {
+        expect(() => {password_is_valid("")}).toThrow(new Error("password should not be null"));
     });
 
-    it ("should have at least one uppercase letter", function(){
-        expect(password_is_valid("123sjkabBcde")).toBe(true);
-    });
+    it("should check password has atleast 1 number", () => {
+        expect(() => {password_is_valid("jhjkDS")}).toThrow(new Error("password should have at least one number"));
+    })
 
-    it ("should have at least one lowercase letter", function(){
-        expect(password_is_valid("sdfjhSJHG48")).toBe(true);
-    });
+    it("should check if password has atleast 1 uppercase letter", () => {
+        expect(() => {password_is_valid("sjhdh")}).toThrow(new Error("password must have atleast 1 uppercase letter"));
+    })
 
-
-    it ("should have at least one number", function(){
-        expect(password_is_valid("KJFD89uiljk")).toBe(true);
-    });
-
-    it ("should have at least one number", function(){
-        expect(password_is_valid("zxcABcde")).toBe(false);
-    });
+    it("should check if password has atleast 1 lowercase", () => {
+        expect(() => {password_is_valid("4585")}).toThrow(new Error("password should have at least one lowercase letter"));
+    })
+        
 });
-
-
-
 
 describe("password_is_ok", function(){
 
@@ -48,11 +40,9 @@ describe("password_is_ok", function(){
         expect(password_is_ok("KJL9548912jkj")).toBe(true);
     });
 
-
     it ("should have at least one number", function(){
         expect(password_is_ok("43678bjhdfjnHJ")).toBe(true);
     });
-
 
     it('should check if password has more than 8 characters and is not null', function(){
         expect(password_is_ok("12345ABcde")).toBe(true);
